f0b3fd9775713349e1fb390cc4784e3ccb7bcf1c
Jonx <dev.jonx.eu@gmail.com>
2023-08-03 02:23:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+
+  data(){
+    return {
+      window: 'main',
+    }
+  },
+  components: {
+      
+  },
+  props: {
+      
+  },
+  methods: {
+      
+  },
+  created() {
+      
+  },
+  mounted(){
+      
+  },
+  watch: {
+      
+  },
+
+}
+</script>
+
+<template>
+
+  <div id="loginWrapper" v-if="window === 'login'">
+    
+  </div>
+
+  <div id="mainWrapper" v-else>
+
+  </div>
+
+</template>
+
+<style scoped>
+    
+</style>
